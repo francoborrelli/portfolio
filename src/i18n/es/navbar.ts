81d2690c1278d15d00d21f47a6b2b6f0eb18c70c
Franco Borrelli <francoborrelli96@outlook.com>
2024-08-24 13:58:13 -0300

    add missing translation

--- src/i18n/es/navbar.ts
+++ src/i18n/es/navbar.ts
@@ -7,4 +7,6 @@ export const navbar = {
   Language: 'Idioma',
   'Contact me': 'Contactame',
   'Source code': 'Código fuente',
+  // SearchPlaceholder: 'What do you want to play?',
+  SearchPlaceholder: '¿Qué quieres escuchar?',
 };
