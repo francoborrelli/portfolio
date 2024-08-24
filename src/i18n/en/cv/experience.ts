30c2678c49788b15e7088ad7a709971ec19dd8ec
Franco Borrelli <francoborrelli96@outlook.com>
2024-08-24 16:53:27 -0300

    update gral information

--- src/i18n/en/cv/experience.ts
+++ src/i18n/en/cv/experience.ts
@@ -9,7 +9,7 @@ const LBS = {
 
 const GRAL = {
   GRAL_JOB_NAME: 'Senior Software Engineer & Devops',
-  GRAL_JOB_LENGTH: 'La Plata, Oct 2019 - Present',
+  GRAL_JOB_LENGTH: 'La Plata, Oct 2019 - Mar 2024',
   GRAL_JOB_DESCRIPTION: `During my work experience, I was involved in the implementation of various web systems for internal company management. In this role, I worked with a variety of frameworks and programming languages, including Typescript (React, Express.js), Python (Django), and PHP (Laravel and Symfony), thus enabling agile and efficient development.\nFurthermore, I was responsible for handling both relational databases, such as MySQL and PostgreSQL, and non-relational databases, including MongoDB and DynamoDB, ensuring the integrity and effectiveness of the systems.\nAnother notable aspect of my work was the implementation of mobile applications using React Native. Additionally, I actively managed these applications on the Google Play Console.\nAs part of my responsibilities, I established and maintained Continuous Integration/Continuous Deployment (CI/CD) pipelines.\nMoreover, I had the opportunity to gain experience in managing infrastructure and services on AWS, including S3, EC2, ECS, CloudWatch, CloudFront, AWS Lambda, and SQS.`,
 };
 
